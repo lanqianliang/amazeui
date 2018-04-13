402a836cdbb47fed169656928245c48b5438f231
lanqianliang <897374333@qq.com>
2018-04-13 17:24:26 +0800

    修改

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 $arr = [1,2,3,4,5,6,7];
 $str = "a,b,c,d,e,f,g";
-$strtoarr = explode(','$str);
+$strtoarr = explode(',',$str);
 $arrtostr = implode(',',$arr);
 $arr_mer = array_merge($arr,$strtoarr);
 $strs = $str.$arrtostr;
